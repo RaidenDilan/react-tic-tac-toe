e9b7d385ddf4b80b165e85298f940806f0e47937
raidendilan <raiden18@me.com>
2018-10-30 17:43:32 +0000

    Completeing the game - lifting state up

--- src/index.js
+++ src/index.js
@@ -5,16 +5,6 @@ import './index.css';
 // Here, Square is a React component class, or React component type
 // The Square component above only renders built-in DOM components like <button /> etc. />
 class Square extends React.Component {
-  // React components can have state by setting this.state in their constructors.
-  constructor(props) {
-    // In JavaScript classes, you need to always call super when defining the constructor of a subclass. All React component classes that have a constructor should start it with a super(props) call.
-    super(props);
-    // Storing the current value of the Square in this.state
-    this.state = {
-      value: null,
-    };
-  }
-
   // The Square component renders a single <button>
   // The render method returns a description of what you want to see on the screen
   render() {
@@ -23,17 +13,49 @@ class Square extends React.Component {
       // When you call setState in a component, React automatically updates the child components inside of it too.
       <button
         className="square"
-        onClick={() => this.setState({ value: 'X' }) }
+        onClick={() => this.props.onClick() }
         >
-        { this.state.value }
+        { this.props.value }
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  // React components can have state by setting this.state in their constructors.
+  constructor(props) {
+    // / In JavaScript classes, you need to always call super when defining the constructor of a subclass. All React component classes that have a constructor should start it with a super(props) call.
+    super(props);
+    // Storing the current value of the Square in this.state
+    this.state = {
+      squares: Array(9).fill(null),
+      // When we fill the board in later, the board will look something like this:
+      // [
+      //   'O', null, 'X',
+      //   'X', 'X', 'O',
+      //   'O', null, null,
+      // ]
+    }
+  };
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({ squares: squares });
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    // Each Square will now receive a value prop that will either be 'X', 'O', or null for empty squares.
+    // return <Square value={ this.state.squares[i] } />;
+
+    // This function will get called when a Square is clicked. We’ll change the renderSquare method in Board to:
+    return (
+      <Square
+        value={ this.state.squares[i] }
+        onClick={ () => this.handleClick(i) }
+      />
+    // We split the returned element into multiple lines for readability, and added parentheses so that JavaScript doesn’t insert a semicolon after return and break our code
+    )
   }
 
   // The Board component renders 9 squares
